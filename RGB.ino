20d1ad6b8cb71e47349b3d7f85446c01a58959b1
Anonymous Newton <99603435+Kwame-Minta2021@users.noreply.github.com>
2022-10-27 07:23:14 -0700

    RGB

--- RGB.ino
+++ RGB.ino
@@ -1,23 +1,33 @@
-int A=7;
-int B =8;
-int C =9;
+//This is the codes for RGB led
+int red_light_pin= 7;
+int green_light_pin = 8;
+int blue_light_pin = 9;
 void setup() {
-    PinMode(A,OUTPUT);
-    PinMode(B,OUTPUT);
-    PinMode(C,OUTPUT);
-    
-
-  // put your setup code here, to run once:
-
+  pinMode(red_light_pin, OUTPUT);
+  pinMode(green_light_pin, OUTPUT);
+  pinMode(blue_light_pin, OUTPUT);
 }
-
 void loop() {
-  digitalWrite(A,HIGH);
-  delay(100);
-  digitalWrite(B,HIGH);
-  delay(100);
-  digitalWrite(C,HIGH)
-  delay(100);
-  // put your main code here, to run repeatedly:
-
+  RGB_color(255, 0, 0); // Red
+  delay(1000);
+  RGB_color(0, 255, 0); // Green
+  delay(1000);
+  RGB_color(0, 0, 255); // Blue
+  delay(1000);
+  RGB_color(255, 255, 125); // Raspberry
+  delay(1000);
+  RGB_color(0, 255, 255); // Cyan
+  delay(1000);
+  RGB_color(255, 0, 255); // Magenta
+  delay(1000);
+  RGB_color(255, 255, 0); // Yellow
+  delay(1000);
+  RGB_color(255, 255, 255); // White
+  delay(1000);
+}
+void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
+ {
+  analogWrite(red_light_pin, red_light_value);
+  analogWrite(green_light_pin, green_light_value);
+  analogWrite(blue_light_pin, blue_light_value);
 }
