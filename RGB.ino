defe00828784df8b1b2ef29bcf53132b074c6f5b
Anonymous Newton <99603435+Kwame-Minta2021@users.noreply.github.com>
2022-10-27 07:19:42 -0700

    Add files via upload

---
+++ RGB.ino
@@ -0,0 +1,23 @@
+int A=7;
+int B =8;
+int C =9;
+void setup() {
+    PinMode(A,OUTPUT);
+    PinMode(B,OUTPUT);
+    PinMode(C,OUTPUT);
+    
+
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(A,HIGH);
+  delay(100);
+  digitalWrite(B,HIGH);
+  delay(100);
+  digitalWrite(C,HIGH)
+  delay(100);
+  // put your main code here, to run repeatedly:
+
+}
